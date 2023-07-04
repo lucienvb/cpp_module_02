b1d23fc68f30facb27681ba2985623ca27ed0ff6
lucienvb <lucienvanbussel@gmail.com>
2023-07-04 16:34:21 +0200

    first exercise in canonical form

---
+++ ex00/include/Fixed.hpp
@@ -0,0 +1,28 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+// INCLUDES
+#include <iostream>
+
+// Canonical from includes the following:
+// 1) A constructor
+// 2) A copy constructor
+// 3) A destructor
+// 4) An assignation operator
+class Fixed {
+public:
+	Fixed();
+	Fixed(const Fixed& other);
+	~Fixed();
+
+	Fixed& operator=(const Fixed& other);
+
+	int		getRawBits(void) const;
+	void	setRawBits(int const raw);
+
+private:
+	int					_value;
+	static const int	_fractionalBits;
+};
+
+#endif
---
+++ ex00/src/Fixed.cpp
@@ -0,0 +1,32 @@
+#include <Fixed.hpp>
+
+Fixed::Fixed() {
+	std::cout << "Default constructor called" << std::endl;
+	_value = 0;
+}
+
+Fixed::Fixed(const Fixed& other) {
+	std::cout << "Copy constructor called" << std::endl;
+	*this = other;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed& Fixed::operator=(const Fixed& other) {
+	std::cout << "Copy assignment operator called" << std::endl;
+	if (this != &other)
+		_value = other._value;
+	return (*this);
+}
+
+int	Fixed::getRawBits() const {
+	std::cout << "getRawBits member function called" << std::endl;
+	return (_value);
+}
+
+void	Fixed::setRawBits(int const raw) {
+	std::cout << "setRawBits member function called" << std::endl;
+	_value = raw;
+}
