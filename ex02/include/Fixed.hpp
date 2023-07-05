3084f281fbb90c7dd23ece74a31fa1c51ecb37e7
lucienvb <lucienvanbussel@gmail.com>
2023-07-05 15:14:42 +0200

    public overload member function finished

--- ex02/include/Fixed.hpp
+++ ex02/include/Fixed.hpp
@@ -32,6 +32,11 @@ public:
 	Fixed&	operator--();
 	Fixed	operator--(int);
 
+	static 			Fixed&	min(Fixed& a, Fixed& b);
+	static const	Fixed	min(const Fixed a, const Fixed b);
+	static 			Fixed&	max(Fixed& a, Fixed& b);
+	static const 	Fixed	max(const Fixed a, const Fixed b);
+
 	float	toFloat(void) const;
 	int 	toInt(void) const;
 
--- ex02/src/Fixed.cpp
+++ ex02/src/Fixed.cpp
@@ -116,3 +116,19 @@ Fixed	Fixed::operator--(int) {
 	--(*this);
 	return (old);
 }
+
+Fixed&	Fixed::min(Fixed& a, Fixed& b) {
+	return (a < b) ? a : b;
+}
+
+const Fixed	Fixed::min(const Fixed a, const Fixed b) {
+	return (a < b) ? a : b;
+}
+
+Fixed&	Fixed::max(Fixed& a, Fixed& b) {
+	return (a > b) ? a : b;
+}
+
+const Fixed	Fixed::max(const Fixed a, const Fixed b) {
+	return (a > b) ? a : b;
+}
