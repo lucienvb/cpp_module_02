3b733858cc61651e0ff43d24823e8d1572a89c39
lvan-bus <lucienvanbussel@gmail.com>
2023-07-19 17:02:11 +0200

    minor fix

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -2,7 +2,6 @@
 
 int main( void ) {
 	Fixed a;
-//	Fixed const b(5.05f * 2);
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
 	std::cout << a << std::endl;
@@ -10,7 +9,6 @@ int main( void ) {
 	std::cout << a << std::endl;
 	std::cout << a++ << std::endl;
 	std::cout << a << std::endl;
-
 
 	std::cout << b << std::endl;
 
